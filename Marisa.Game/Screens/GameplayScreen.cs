235de4daf34d690ea3861478db611487fc15beb7
Eevee <developerbrzoza@gmail.com>
2020-11-30 20:01:13 +0100

    Added Screen Support and Gameplay moved to a New Class based off IMarisaScreen

---
+++ Marisa.Game/Screens/GameplayScreen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Marisa.Game.Audio;
+using Marisa.Game.GameObjects;
+using Marisa.Game.GameObjects.Enums;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osuTK.Graphics;
+
+namespace Marisa.Game.Screens
+{
+    class GameplayScreen : IMarisaScreen
+    {
+        private Beatmap map = Beatmap.FromJson(File.ReadAllText("charts/messed up gravity/diff.json"));
+        private AudioPlayback playback = new AudioPlayback("charts/messed up gravity/audio.mp3");
+
+        public GameplayScreen(MarisaGame ctx)
+        {
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    Colour = Color4.DimGray,
+                    RelativeSizeAxes = Axes.Both,
+                },
+                new SpriteText
+                {
+                    Y = 20,
+                    Text = "Main Screen",
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
+                    Font = FontUsage.Default.With(size: 40)
+                },
+
+            };
+
+            foreach (Hit h in map.Hits)
+            {
+                AddInternal(new DrumHit(h.Time, (HitColor)h.Color));
+            }
+
+            ctx.AddAudioItem(playback.GetAudioComponent());
+
+            LoadComplete();
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            //Starts Audio
+            playback.Start();
+            //Currently stuff's broken so this is here
+            map.LogBeatmap();
+        }
+    }
+}
---
+++ Marisa.Game/Screens/IMarisaScreen.cs
@@ -0,0 +1,42 @@
+﻿using osu.Framework.Screens;
+using osu.Framework.Graphics;
+
+namespace Marisa.Game.Screens
+{
+    public class IMarisaScreen : Screen
+    {
+        public IMarisaScreen()
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+        }
+
+        public override void OnEntering(IScreen last)
+        {
+            base.OnEntering(last);
+            this.FadeInFromZero(200, Easing.Out);
+        }
+
+        public override bool OnExiting(IScreen next)
+        {
+            this.FadeOut(200, Easing.Out);
+            this.ScaleTo(0.9f, 200, Easing.Out);
+            return base.OnExiting(next);
+        }
+
+        public override void OnSuspending(IScreen next)
+        {
+            base.OnSuspending(next);
+            this.ScaleTo(1.1f, 200, Easing.Out);
+            this.FadeOut(200, Easing.Out);
+        }
+
+        public override void OnResuming(IScreen last)
+        {
+            base.OnResuming(last);
+
+            this.ScaleTo(1, 200, Easing.Out);
+            this.FadeIn(200, Easing.Out);
+        }
+    }
+}
